679be1381f026e86aaa6b8b8847606b85f014afd
Malde <maldefrankmidtgaard@gmail.com>
2026-05-06 12:00:44 +0200

    fix

--- robot/claw_control.py
+++ robot/claw_control.py
@@ -27,16 +27,12 @@ class Claw:
             return True
         return False
 
-    def reset_claw(self, speedDPS=20, timeout_s=3):
-        """Drive motor until stalled, then zero the position.
-        timeout_s guards against infinite loop if stall is never detected."""
-        import time
-        self.motor.on(speed=SpeedDPS(speedDPS))
-        deadline = time.time() + timeout_s
-        while time.time() < deadline:
-            if self.motor.is_stalled:
-                break
-        self.motor.stop(stop_action='hold')
+    def reset_claw(self, speedDPS=20):
+        self.motor.on(speed=SpeedDPS(speedDPS)) 
+        while self.motor.speed > 1:
+            pass 
+        
+        self.motor.stop(stop_action='hold') 
         self.motor.position = 0
         print("Claw is reset")
         return True

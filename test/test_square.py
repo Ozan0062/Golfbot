72070459b05473c4ff3f186192db759b9f46d186
s163961 <s163961@student.dtu.dk>
2026-06-10 14:53:03 +0200

    70p and 50p

--- test/test_square.py
+++ test/test_square.py
@@ -3,12 +3,11 @@ sys.path.insert(0, os.path.expanduser("~/"))
 
 from mov_control import drive_forward, turn_right
 
-FORWARD_ROTATIONS = 5.0
+FORWARD_ROTATIONS = 6.0
 TURN_ROTATIONS    = 3.0   # nudge until corners are 90 degrees
 
 for _ in range(4):
     drive_forward(FORWARD_ROTATIONS)
     turn_right(TURN_ROTATIONS)
-    drive_reverse(FORWARD_ROTATIONS)
 
 print("Done.")

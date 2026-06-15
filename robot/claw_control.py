2c51716b7b3ddae5557f7109b426b12dc56a67ae
s163961 <s163961@student.dtu.dk>
2026-06-15 09:18:17 +0200

    forward test

--- robot/claw_control.py
+++ robot/claw_control.py
@@ -10,7 +10,7 @@ class Claw:
         self.motor = motor(port)
         self.gate = gate
 
-    def close_claw(self, speedDPS=-1560, seconds=1):
+    def close_claw(self, speedDPS=-300, seconds=1):
         self.motor.on_for_seconds(
             speed=SpeedDPS(speedDPS),
             seconds=seconds,
@@ -19,13 +19,13 @@ class Claw:
         )
         return True
 
-    def open_claw(self, speedDPS=1560):
+    def open_claw(self, speedDPS=300):
         self.motor.on(speed=SpeedDPS(speedDPS))
         self.motor.wait_until('stalled')
         self.motor.stop(stop_action='brake')
         return True
 
-    def reset_claw(self, speedDPS=1560):
+    def reset_claw(self, speedDPS=300):
         self.motor.on(speed=SpeedDPS(speedDPS))
         self.motor.wait_until('stalled')
         self.motor.stop(stop_action='brake')
